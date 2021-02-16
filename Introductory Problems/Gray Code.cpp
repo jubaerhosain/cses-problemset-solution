effc7d25b02c5f0ae92d0e8f84028a1b59b99776
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-02-16 12:13:04 +0600

    Create Gray Code.cpp

---
+++ Introductory Problems/Gray Code.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define rall(v)                 v.rbegin(), v.rend()
+#define des()                   greater<int>()
+#define rep(i, a, n)            for(int i = a; i < n; i++)  
+#define rrep(i, n, a)           for(int i = n; i >= a; i--)
+#define pqueue                  priority_queue
+#define umap                    unordered_map
+#define uset                    unordered_set
+#define pub                     push_back
+#define pob                     pop_back  
+#define endl                    '\n' //in interactive problem should not use this
+#define println                 cout << endl
+#define scan_array(arr, n)      for(int i = 0; i < n; i++) { cin >> arr[i]; }
+#define print_array(arr, n)     for(int i = 0; i < n; i++) { cout << arr[i] << " "; } println;
+#define print(var)              cout << var << endl
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef pair<int, int> pii; 
+typedef pair<ll, ll> pll; 
+const int p = 1e9+7;
+const int eps = 1e-9;
+const int inf = INT_MAX;
+const int N = 2e5+10;
+
+
+/*************User defined function*************/
+void solve() {
+    int n;
+    cin >> n;
+
+    rep(i, 0, (1<<n)) { 
+        int ans = i^(i>>1);
+        string s(n, '0');
+        rep(j, 0, n) {
+            if((ans>>j)&1) s[j] = '1';
+            else s[j] = '0';
+        }
+        reverse(all(s));
+        print(s);
+    }
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    // int test;
+    // cin >> test;
+    // while(test-- > 0) {
+    //     solve();
+    // }
+
+    solve();
+
+    return 0;
+}    

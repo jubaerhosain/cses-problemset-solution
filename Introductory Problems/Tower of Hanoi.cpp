1f5f0a579c98a56f2ed9c0bba4f8961d359bc0ff
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-02-16 15:11:17 +0600

    Create Tower of Hanoi.cpp

---
+++ Introductory Problems/Tower of Hanoi.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define all(v)                  v.begin(), v.end()
+#define rall(v)                 v.rbegin(), v.rend()
+#define des()                   greater<int>()
+#define rep(i, a, n)            for(int i = a; i < n; i++)  
+#define rrep(i, n, a)           for(int i = n; i >= a; i--)
+#define pqueue                  priority_queue
+#define umap                    unordered_map
+#define uset                    unordered_set
+#define pub                     push_back
+#define pob                     pop_back  
+#define endl                    '\n' //in interactive problem should not use this
+#define println                 cout << endl
+#define scan_array(arr, n)      for(int i = 0; i < n; i++) { cin >> arr[i]; }
+#define print_array(arr, n)     for(int i = 0; i < n; i++) { cout << arr[i] << " "; } println;
+#define print(var)              cout << var << endl
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef long double ld;
+typedef pair<int, int> pii; 
+typedef pair<ll, ll> pll; 
+const int p = 1e9+7;
+const int eps = 1e-9;
+const int inf = INT_MAX;
+const int N = 2e5+10;
+
+
+/*************User defined function*************/
+void toh(int n, int a, int b, int c) {
+    if(n == 0) return;
+    toh(n-1, a, c, b);
+    cout << a << " " << c << endl;
+    toh(n-1, b, a, c);
+}
+
+void solve() {
+    int n; cin >> n;
+
+    print((1<<n)-1);
+    toh(n, 1, 2, 3);
+}
+
+/*************main function*************/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+
+    // int test;
+    // cin >> test;
+    // while(test-- > 0) {
+    //     solve();
+    // }
+
+    solve();
+
+    return 0;
+}    

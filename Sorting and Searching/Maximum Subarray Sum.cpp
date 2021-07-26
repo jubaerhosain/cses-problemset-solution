06803c33a8b45089242a22cc227842195c1eed4c
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-07-26 18:49:51 +0600

    Create Maximum Subarray Sum.cpp

---
+++ Sorting and Searching/Maximum Subarray Sum.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl          '\n'//notforinter
+#define all(v)        v.begin(), v.end()
+#define print(var)    cout << var << endl
+#define rep(i, s, n)  for(int i = s; i <= n; i++)
+#define rrep(i, n, s) for(int i = n; i >= s; i--)
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+
+/*****User defined function*****/
+const int N = 2e5+5;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    rep(i, 0, n-1) cin >> a[i];
+
+    ll mx = a[0], curr = a[0];
+    rep(i, 1, n-1) {
+        curr = max(curr + a[i], (ll)a[i]);
+        mx = max(mx, curr);
+    }
+
+    print(mx);
+} 
+ 
+/*****main function*****/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+ 
+    cout << fixed << showpoint;
+    cout << setprecision(16);
+ 
+    int test = 1;
+    //cin >> test;
+    while(test--) {
+        solve();
+    }
+ 
+    return 0;
+} 

12875c3812850d59491310a763ec4f78a5683d09
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-07-26 19:32:53 +0600

    Create Stick Lengths.cpp

---
+++ Sorting and Searching/Stick Lengths.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl          '\n'//notforinter
+#define all(v)        v.begin(), v.end()
+#define print(var)    cout << var << endl
+#define rep(i, s, n)  for(int i = s; i <= n; i++)
+#define rrep(i, n, s) for(int i = n; i >= s; i--)
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+
+/*****User defined function*****/
+const int N = 2e5+5;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    rep(i, 0, n-1) cin >> a[i];
+
+    sort(all(a));
+
+    ll sum = 0, mid = a[n/2];
+    rep(i, 0, n-1) sum += abs(mid-a[i]);
+
+    print(sum);
+} 
+ 
+/*****main function*****/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif 
+ 
+    cout << fixed << showpoint;
+    cout << setprecision(16);
+ 
+    int test = 1;
+    //cin >> test;
+    while(test--) {
+        solve();
+    }
+ 
+    return 0;
+} 

ba8c5fc0cc0ad8498dd5bba5c5aedbdc48afde94
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-08-05 19:44:30 +0600

    Create Common Divisors.cpp

---
+++ Mathematics/Common Divisors.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl          '\n'//not for interactive
+#define rep(i, s, n)  for(int i = s; i <= n; i++)
+#define rrep(i, n, s) for(int i = n; i >= s; i--)
+typedef long long ll;
+typedef pair<int, int> pii;
+
+/*****User defined function*****/
+int mod = 1e9+7;
+const int N = 1e6+5;
+int freq[N];
+
+void solve() {
+    int n;
+    cin >> n;
+
+    rep(i, 0, n-1) {
+        int x;
+        cin >> x;
+        freq[x]++;
+    }
+
+    rrep(i, N-5, 1) {
+        int cnt = 0;
+        for(int j = i; j < N; j += i) 
+            cnt += freq[j];
+            
+        if(cnt > 1) {
+            cout << i << endl;
+            break;
+        }
+    }
+}
+
+/*****main function*****/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    cout << fixed << showpoint;
+    cout << setprecision(16);
+
+    int test = 1;
+    //cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+} 

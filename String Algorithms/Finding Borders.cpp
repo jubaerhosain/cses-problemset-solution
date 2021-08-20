ccf1431544499066026b66ee46bcb2008dcbdd5c
Igloo11 <61691077+JubaerHosain@users.noreply.github.com>
2021-08-20 20:51:07 +0600

    Create Finding Borders.cpp

---
+++ String Algorithms/Finding Borders.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl    '\n'
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<vector<int>> vii;
+
+/*****User defined function*****/
+int mod = 1e9+7;
+const int N = 1e5+5;
+
+auto zfunction(string &str) {
+    int n = str.size();
+
+    int l = 0, r = 0;
+    vector<int> z(n);
+    for(int i = 1; i < n; i++) {
+        if(i > r) {
+            l = i, r = i;
+            while(r < n && str[r-l] == str[r])
+                r++;
+            r--;
+            z[i] = r - l + 1;
+        } else {
+            int j = i - l;
+            if(z[j] < r-i+1) {
+                z[i] = z[i];
+            } else {
+                l = i;
+                while(r < n && str[r-l] == str[r])
+                    r++;
+                r--;
+                z[i] = r - l + 1;
+            }
+        }
+    }
+        
+    return z;
+}
+
+void solve_task() {
+    string text;
+    cin >> text;
+
+    auto z = zfunction(text);
+
+    int n = text.size();
+    for(int i = n-1; i >= 0; i--) 
+        if(z[i] == n-i) cout << n-i << " ";
+}
+
+/*****main function*****/
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    cout << fixed << showpoint;
+    cout << setprecision(16);
+
+    int test = 1;
+    //cin >> test;
+    while(test--) {
+        solve_task();
+    }
+
+    return 0;
+} 
